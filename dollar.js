58a802be2887dd3fbbb8dbc5c3cafc650241fc5c
Antti Mattila <antti.mattila@reaktor.fi>
2013-05-01 11:05:28 +0300

    Use jsdom by default

--- dollar.js
+++ dollar.js
@@ -1,16 +1,43 @@
 var Q = require("q")
 var cheerio = require("cheerio")
 var request = require("request")
+var jsdom = require("jsdom")
+var defaults = {
+  fullJQuery: true
+}
 
-exports.get = function(url) {
+exports.get = function(url, options) {
+  var opts = options || defaults
   var deferred = Q.defer()
   request(url, function(err, resp, body){
     if (err) {
       deferred.reject(err)
     } else {
-      var $ = cheerio.load(body)
-      deferred.resolve($)
+      if (opts.fullJQuery) {
+        jsdomify(body, deferred);
+      } else {
+        cheerify(body, deferred);
+      }
     }
   })
   return deferred.promise
+}
+
+function cheerify(body, deferred) {
+  var $ = cheerio.load(body)
+  deferred.resolve($)
+}
+
+function jsdomify(body, deferred) {
+  jsdom.env(
+    body,
+    ['http://code.jquery.com/jquery.js'],
+    function(errors, window) {
+      if (errors) {
+        deferred.reject(errors)
+      } else {
+        deferred.resolve(window.$)
+      }
+    }
+  )
 }
--- example.js
+++ example.js
@@ -2,5 +2,9 @@ var dollar = require('./dollar')
 console.log('Start..')
 
 dollar.get('http://reaktor.fi/').then(function($) {
-  console.log($('h1').text())
+  console.log('With full jQuery:', $('h1').text())
+})
+
+dollar.get('http://reaktor.fi/', {fullJQuery: false}).then(function($) {
+  console.log('With faster jQuery like:', $('h1').text())
 })
