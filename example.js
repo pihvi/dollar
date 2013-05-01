e62144fb1665b0978d67f89916772e6d44487c9b
Antti Mattila <antti.mattila@reaktor.fi>
2013-05-01 12:43:02 +0300

    Patch cheerio to work with data() on data-attributes

--- example.js
+++ example.js
@@ -1,10 +1,15 @@
 var dollar = require('./dollar')
 console.log('Start..')
 
-dollar.get('http://reaktor.fi/').then(function($) {
-  console.log('With full jQuery:', $('h1').text())
+dollar.get('http://www.hs.fi/').then(function($) {
+  console.log('With full jQuery:\t\t', extractData($))
 })
 
-dollar.get('http://reaktor.fi/', {fullJQuery: false}).then(function($) {
-  console.log('With faster jQuery like:', $('h1').text())
+dollar.get('http://www.hs.fi/', {fullJQuery: false}).then(function($) {
+  console.log('With faster jQuery like:\t', extractData($))
 })
+
+function extractData($) {
+  var a = $('.module.teaser').first().find('a.article-link').first()
+  return a.text().trim() + ' / ' + a.data('article-id')
+}
