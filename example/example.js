69e7ecdeb9ee6df50c5024011b4471c8342fd34e
Antti Mattila <antti.mattila@reaktor.fi>
2014-03-30 12:19:56 +0300

    remove tabs

--- example/example.js
+++ example/example.js
@@ -6,26 +6,26 @@ var start = Date.now()
 console.log('Start..')
 
 dollar.get('http://www.hs.fi/').then(function($) {
-  console.log('With full jQuery and internet:\t', extractData($))
+  console.log('With full jQuery and internet:', extractData($))
 })
 
 dollar.get({url: 'http://www.hs.fi/', fullJQuery: false}).then(function($) {
-  console.log('With faster and internet:\t\t', extractData($))
+  console.log('With faster and internet:', extractData($))
 })
 
 dollar.get({html: hs, fullJQuery: false}).then(function($) {
-  console.log('With faster local:\t', extractData($))
+  console.log('With faster local:', extractData($))
 })
 
 dollar.get({html: hs}).then(function($) {
-  console.log('With full jQuery local:\t', extractData($))
+  console.log('With full jQuery local:', extractData($))
 })
 
 dollar.get({htmlPromise: hsPromise, fullJQuery: false}).then(function($) {
-  console.log('With faster promised local html:\t', extractData($))
+  console.log('With faster promised local html:', extractData($))
 })
 
 function extractData($) {
   var a = $('.module.teaser').first().find('a.article-link').first()
-  return a.text().trim() + ' / ' + a.attr('data-article-id') + ' (id of type: ' + (typeof a.attr('data-article-id')) + ') in ' + (Date.now() - start) + 'ms'
+  return a.text().trim() + ' / ' + a.attr('data-article-id') + (Date.now() - start) + 'ms'
 }
