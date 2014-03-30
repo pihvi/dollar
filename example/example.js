d76d34dfdf008b97451342eb49f320c354c715fe
Antti Mattila <antti.mattila@reaktor.fi>
2014-03-30 14:24:32 +0300

    Format messages

--- example/example.js
+++ example/example.js
@@ -6,26 +6,38 @@ var start = Date.now()
 console.log('Start..')
 
 dollar.get('http://www.hs.fi/').then(function($) {
-  console.log('With full jQuery and internet:', extractData($))
+  console.log(buildOutput('With full jQuery and internet', $, start))
 })
 
 dollar.get({url: 'http://www.hs.fi/', fullJQuery: false}).then(function($) {
-  console.log('With faster and internet:', extractData($))
+  console.log(buildOutput('With faster and internet', $, start))
 })
 
 dollar.get({html: hs, fullJQuery: false}).then(function($) {
-  console.log('With faster local:', extractData($))
+  console.log(buildOutput('With faster local', $, start))
 })
 
 dollar.get({html: hs}).then(function($) {
-  console.log('With full jQuery local:', extractData($))
+  console.log(buildOutput('With full jQuery local', $, start))
 })
 
 dollar.get({htmlPromise: hsPromise, fullJQuery: false}).then(function($) {
-  console.log('With faster promised local html:', extractData($))
+  console.log(buildOutput('With faster promised local html', $, start))
 })
+
+function buildMsg(msg) {
+  var result = msg + ':'
+  while (result.length < 35) {
+    result += ' '
+  }
+  return result
+}
+
+function buildOutput(msg, $, begin) {
+  return buildMsg(msg) + extractData($) + ' / ' + (Date.now() - begin) + 'ms'
+}
 
 function extractData($) {
   var a = $('.module.teaser').first().find('a.article-link').first()
-  return a.text().trim() + ' / ' + a.attr('data-article-id') + (Date.now() - start) + 'ms'
+  return a.text().trim() + ' / ' + a.attr('data-article-id')
 }
