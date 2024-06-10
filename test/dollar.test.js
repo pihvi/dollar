3c1fbffba17af2906143501c1741bdd6add1602c
Antti Mattila <72422+pihvi@users.noreply.github.com>
2024-06-10 18:39:06 +0300

    test jQuery with jsdom

--- test/dollar.test.js
+++ test/dollar.test.js
@@ -1,20 +1,20 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
-//import * as dollar from '../lib/dollar.js'
 import {JSDOM} from 'jsdom'
 import {promises as fs} from 'fs'
 
-test('jsdom init', (t) => {
+test('jsdom init', () => {
   const html = '<html lang=""><body><h1>lol</h1></body></html>'
   const dom = new JSDOM(html)
   const h1 = dom.window.document.querySelector('h1').textContent
   assert.equal(h1, 'lol')
 })
 
-test('jsdom jQuery', async (t) => {
+test('jsdom jQuery', async () => {
   const html = '<html lang=""><body><h1>lol</h1></body></html>'
   const dom = new JSDOM(html, {runScripts: 'outside-only'})
-  const jQuery = await fs.readFile('./node_modules/jquery/dist/jquery.js', 'utf-8')
+  const jQueryPath = './node_modules/jquery/dist/jquery.js'
+  const jQuery = await fs.readFile(jQueryPath, 'utf-8')
   dom.window.eval(jQuery)
   const h1 = dom.window.$('h1').text()
   assert.equal(h1, 'lol')
