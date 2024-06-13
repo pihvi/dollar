f454b8ac6795d9fda7863636c93b296da4a35560
Antti Mattila <72422+pihvi@users.noreply.github.com>
2024-06-13 19:22:31 +0300

    test dollar with local html and cheerio and jQuery

--- test/dollar.test.js
+++ test/dollar.test.js
@@ -1,7 +1,8 @@
 import {before, describe, test} from 'node:test'
 import assert from 'node:assert/strict'
-import {JSDOM} from 'jsdom'
 import {promises as fs} from 'fs'
+import {JSDOM} from 'jsdom'
+import * as dollar from './../lib/dollar.js'
 
 describe('with example html', () => {
   let html
@@ -9,25 +10,32 @@ describe('with example html', () => {
     html = await fs.readFile('./example/wikipedia.html', 'utf-8')
   })
 
-  test('jsdom init', () => {
-    const dom = new JSDOM(html)
-    const h1 = dom.window.document.querySelector('h1').textContent.trim()
-    assert.equal(h1, 'The Free Encyclopedia')
-  })
-
-  test('jsdom jQuery', async () => {
-    const dom = new JSDOM(html, {runScripts: 'outside-only'})
-    const jQueryPath = './node_modules/jquery/dist/jquery.js'
-    const jQuery = await fs.readFile(jQueryPath, 'utf-8')
-    dom.window.eval(jQuery)
-    const h1 = dom.window.$('h1').text().trim()
-    assert.equal(h1, 'The Free Encyclopedia')
-  })
-
   test('dollar local faster', async () => {
-    const html = '<html lang=""><body><h1>lol</h1></body></html>'
-    const dom = new JSDOM(html)
-    const h1 = dom.window.document.querySelector('h1').textContent
-    assert.equal(h1, 'lol')
+    const $ = await dollar.get({html})
+    const h1 = $('h1').text().trim()
+    assert.equal(h1, 'The Free Encyclopedia')
+  })
+
+  test('dollar local jQuery', async () => {
+    const $ = await dollar.get({html, fullJQuery: true})
+    const h1 = $('h1').text().trim()
+    assert.equal(h1, 'The Free Encyclopedia')
+  })
+
+  describe('just JSDOM', () => {
+    test('jsdom init', () => {
+      const dom = new JSDOM(html)
+      const h1 = dom.window.document.querySelector('h1').textContent.trim()
+      assert.equal(h1, 'The Free Encyclopedia')
+    })
+
+    test('jsdom jQuery', async () => {
+      const dom = new JSDOM(html, {runScripts: 'outside-only'})
+      const jQueryPath = './node_modules/jquery/dist/jquery.js'
+      const jQuery = await fs.readFile(jQueryPath, 'utf-8')
+      dom.window.eval(jQuery)
+      const h1 = dom.window.$('h1').text().trim()
+      assert.equal(h1, 'The Free Encyclopedia')
+    })
   })
 })
